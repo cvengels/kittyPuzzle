0975a76132471b1350abf2b8dfd1bac2c6cc2d78
C.Vengels <6281096+cvengels@users.noreply.github.com>
2021-02-08 15:22:43 +0100

    Beweung von normalen Kisten gefixt

--- Assets/Scripts/ObjectGridInteraction.cs
+++ Assets/Scripts/ObjectGridInteraction.cs
@@ -10,7 +10,7 @@ public class ObjectGridInteraction : MonoBehaviour
 
     private bool isMoving;
     private bool pushing;
-    private float movementSpeedOverwrite;
+    private float newMovementSpeedCalculated;
 
     private Vector3 newMovePosition;
 
@@ -103,19 +103,26 @@ public class ObjectGridInteraction : MonoBehaviour
     }
 
 
-    void Update()
+    void FixedUpdate()
     {
         if (isMoving)
         {
-            if (!pushing) // normal movement of e.g. player
+            if (pushing) // when pushing one or more objects, this gameobject gets a new speed and moves linearly
             {
-                transform.position = Vector3.Lerp(transform.position, newMovePosition, movementSpeed * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    newMovePosition,
+                    newMovementSpeedCalculated * Time.fixedDeltaTime);
             }
-            else // when pushing one or more objects, this gameobject gets a new speed and moves linearly
+            else // normal movement, e.g. of player or NPC
             {
-                transform.position = Vector3.MoveTowards(transform.position, newMovePosition, movementSpeedOverwrite * Time.deltaTime);
+                transform.position = Vector3.MoveTowards(
+                    transform.position,
+                    Vector3.Lerp(transform.position, newMovePosition, movementSpeed / 10),
+                    newMovementSpeedCalculated * Time.fixedDeltaTime);
             }
 
+            // If own position near target position, move directly to new position and quit moving task
             if (Vector3.Distance(transform.position, newMovePosition) < 0.02f)
             {
                 isMoving = false;
@@ -131,68 +138,128 @@ public class ObjectGridInteraction : MonoBehaviour
     }
 
 
-    public bool AskToMove(Vector2 direction)
+    public float AskToMove(Vector2 direction, float forwardedSpeed, int pushOrder = 0)
+    {
+        if (!isMoving)
+        {
+            newMovementSpeedCalculated = movementSpeed;
+            if (CanMoveOnGrid(direction))
+            {
+                // set new position to move to and check if other GameObjects are on this position
+                newMovePosition = transform.position + (Vector3)direction;
+                List<GameObject> objectsInPath = CheckForObjectsOnTargetPosition(newMovePosition);
+
+                if (objectsInPath.Count == 0)
+                {
+                    isMoving = true;
+                    pushing = false;
+                    return newMovementSpeedCalculated;
+                }
+                else if (objectsInPath.Count == 1)
+                {
+                    // Get reference of object in front of this
+                    ObjectGridInteraction objectInFrontOfMe = objectsInPath[0].GetComponent<ObjectGridInteraction>();
+                    // If GameObject is player, you are first in push order. Otherwise increment value
+                    pushOrder = this.gameObject.CompareTag("Player") ? 0 : pushOrder++;
+
+                    // Is object in front of me pushable?
+                    if (objectInFrontOfMe.IsPushable)
+                    {
+                        // get movement speed of object in front of me
+                        newMovementSpeedCalculated = objectInFrontOfMe.AskToMove(direction, newMovementSpeedCalculated, pushOrder);
+                        newMovementSpeedCalculated = Mathf.Min(newMovementSpeedCalculated, this.movementSpeed);
+
+                        if (newMovementSpeedCalculated > 0f)
+                        {
+                            // Is it heavy (only pushable alone, no other objects in front or behind it
+                            if (objectInFrontOfMe.IsHeavy)
+                            {
+                                // ToDo
+                            }
+                            else // Not heavy
+                            {
+                                isMoving = true;
+                                pushing = true;
+                                return newMovementSpeedCalculated;
+                            }
+                        }
+                        else
+                        {
+                            ResetTargetPosition();
+                            return 0f;
+                        }
+                    }
+                }
+                else // more than one object, e.g. a button, hole and a box on top
+                {
+                    //ToDo
+                }
+            }
+            else
+            {
+                ResetTargetPosition();
+                return 0f;
+            }
+        }
+        return newMovementSpeedCalculated;
+    }
+
+
+    private void ResetTargetPosition()
+    {
+        newMovePosition = transform.position;
+    }
+    /*
+    public float AskToMove(Vector2 direction, float forwardedSpeed)
     {
         if (!isMoving)
         {
             if (CanMoveOnGrid(direction))
             {
                 newMovePosition = transform.position + (Vector3)direction;
-                if (CheckForObjectsOnTarget(newMovePosition).Count == 0)
+                List<GameObject> objectsInPath = CheckForObjectsOnTargetPosition(newMovePosition);
+
+                if (objectsInPath.Count == 0)
                 {
-                    //Debug.Log(this.name + " soll von " + transform.position.ToString() + " zu " + newMovePosition.ToString());
                     isMoving = true;
                     if (this.transform.CompareTag("Player"))
                     {
                         EventManager.current.PlayerStartMove();
                     }
-                    return true;
+                    return 0f;
 
                 }
-                else
+                else if (objectsInPath.Count == 1)
                 {
-                    List<GameObject> objectsInPath = CheckForObjectsOnTarget(newMovePosition);
-                    /*
-                    string objectNames = "";
-                    foreach (GameObject go in objectsInPath)
+                    ObjectGridInteraction objectInFrontOfMe = objectsInPath[0].GetComponent<ObjectGridInteraction>();
+                    movementSpeedOverwrite = movementSpeed > forwardedSpeed ? movementSpeed : forwardedSpeed;
+
+                    if (objectInFrontOfMe.AskToMove(direction, movementSpeedOverwrite) > 0f && objectInFrontOfMe.isPushable)
                     {
-                        objectNames += go.name + " ";
-                    }
-                    Debug.Log(this.name + ": Objekte vor mir: " + objectNames);
-                    */
-                    if (objectsInPath.Count == 1)
-                    {
-                        ObjectGridInteraction objectInFrontOfMe = objectsInPath[0].GetComponent<ObjectGridInteraction>();
-                        if (objectInFrontOfMe.AskToMove(direction) && objectInFrontOfMe.isPushable)
-                        {
-                            movementSpeedOverwrite = objectInFrontOfMe.movementSpeed < this.movementSpeed ? 
-                                objectInFrontOfMe.movementSpeed : this.movementSpeed;
-                            isMoving = true;
-                            pushing = true;
-                            return true;
-                        }
+                        isMoving = true;
+                        pushing = true;
+                        return movementSpeedOverwrite;
                     }
                 }
             }
         }
-        return false;
+        newMovePosition = transform.position;
+        return 0f;
     }
-
+    */
 
     private bool CanMoveOnGrid(Vector2 direction)
     {
         Vector3Int gridTargetPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
         if (!groundTilemap.HasTile(gridTargetPosition) || collisionTilemap.HasTile(gridTargetPosition))
         {
-            //string nameOfTile = collisionTilemap.GetTile(gridPosition).name;
-            //Debug.Log("Hindernis heißt " + nameOfTile);
             return false;
         }
         return true;
     }
 
 
-    private List<GameObject> CheckForObjectsOnTarget(Vector3 targetPosition)
+    private List<GameObject> CheckForObjectsOnTargetPosition(Vector3 targetPosition)
     {
         // https://answers.unity.com/questions/383671/find-gameobject-at-position.html
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,29 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     ObjectGridInteraction gridInteractor;
+    ObjectGridInteraction playerObject;
 
     private void Start()
     {
         gridInteractor = GetComponent<ObjectGridInteraction>();
+        playerObject = GetComponent<ObjectGridInteraction>();
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            gridInteractor.AskToMove(Vector2.up);
+            gridInteractor.AskToMove(Vector2.up, playerObject.MovementSpeed);
         }
         else if (Input.GetKeyDown(KeyCode.A))
         {
-            gridInteractor.AskToMove(Vector2.left);
+            gridInteractor.AskToMove(Vector2.left, playerObject.MovementSpeed);
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            gridInteractor.AskToMove(Vector2.down);
+            gridInteractor.AskToMove(Vector2.down, playerObject.MovementSpeed);
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
-            gridInteractor.AskToMove(Vector2.right);
+            gridInteractor.AskToMove(Vector2.right, playerObject.MovementSpeed);
         }
     }
 
